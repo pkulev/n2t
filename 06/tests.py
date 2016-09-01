019575ec1afd7a86e730f6d79282ae66c66c515d
Pavel Kulyov <kulyov.pavel@gmail.com>
2016-09-01 19:51:33 +0300

    Add some tests

--- 06/tests.py
+++ 06/tests.py
@@ -1,17 +1,82 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(__file__))
+"""HASM unit tests."""
+
+import imp
+from collections import MutableMapping
 
 import pytest
 
-import hasm
+
+hasm = imp.load_source("hasm", "./hasm")  # noqa
 
 
-@pytest.mark.parametrize(("filters", "data", "expected"), (
+def test_create_builtins_symbol_table():
+
+    assert isinstance(hasm.create_builtins_symbol_table(), MutableMapping)
+
+
+def test_symbol_table():
+    pass
+
+
+def test_is_type():
+    pass
+
+
+@pytest.mark.parametrize(("symbol", "expected"), (
+    (0, "0000000000000000"),
+    (16, "0000000000010000"),
+    ("17", "0000000000010001"),
+    (65536, "10000000000000000"),
+))
+def test_to_word(symbol, expected):
+    assert hasm.to_word(symbol) == expected
+
+
+def test_ralloc():
+    pass
+
+
+def test_remove_inline_comment():
+    pass
+
+
+def test_parse_a_instruction():
+    pass
+
+
+def test_parse_c_instruction():
+    pass
+
+
+def test_process_label_declaration():
+    pass
+
+
+def test_expand_labels():
+    pass
+
+
+def test_assembler():
+    pass
+
+
+def test_parse_args():
+    pass
+
+
+def test_prepare_filenames():
+    pass
+
+
+def test_main():
+    pass
+
+
+@pytest.mark.parametrize(("functions", "data", "expected"), (
     ([], [], []),
     ([lambda x: x + 1], list(range(0, 5)), list(range(1, 6))),
-    ([lambda x: x + 1, lambda x: x * 2], range(0, 5), range(2, 10, 2)),
+    ([lambda x: x + 1, lambda x: x * 2], range(0, 5), range(2, 12, 2)),
 ))
-def test_apply_filters(filters, data, expected):
+def test_apply_functions(functions, data, expected):
 
-    assert hasm.apply_filters(filters, data) == expected
+    assert hasm.apply_functions(functions, data) == expected
